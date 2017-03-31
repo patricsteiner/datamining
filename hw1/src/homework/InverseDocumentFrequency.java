23eb6f6218b5aa9b0ac4de8faf0f8e257a7c93c0
Patric Steiner <patric.steiner1@gmail.com>
2017-03-31 16:37:47 +0200

    completed task 2 (inverse document frequency) --> it actually worked perfectly the first time i built :O i'm a god

--- hw1/src/homework/InverseDocumentFrequency.java
+++ hw1/src/homework/InverseDocumentFrequency.java
@@ -9,6 +9,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -17,6 +18,8 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class InverseDocumentFrequency extends Configured implements Tool {
     public static void main(String[] args) throws Exception {
@@ -58,10 +61,47 @@ public class InverseDocumentFrequency extends Configured implements Tool {
         return job.waitForCompletion(true) ? 0 : -1;
     }
 
+    /**
+     * Mapper Implementation for InverseDocumentFrequency Job
+     *
+     * Input:  key   - a document number (type: LongWritable)
+     *         value - each word in the document (type: Text)
+     * Output: key   - a word (type: Text)
+     *         value - 1 (type: IntWritable, not used)
+     */
     public static class IDFMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-        // fill your code here!
+    	private Text keyOut = new Text();
+        private IntWritable one = new IntWritable(1);
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        	// split line by a space character
+            String line = value.toString();
+            line = line.substring(line.indexOf('\t') + 1); // only use the words, ignore the document number
+            String[] words = line.split(" ");
+
+            // use a set to save all words that have already been written
+            Set<String> alreadyWritten = new HashSet<>();
+            
+            for (String word : words) {
+                // write (word, 1) once for each word in the line
+            	if (!alreadyWritten.contains(word)) {
+	            	alreadyWritten.add(word);
+	                keyOut.set(word);
+	                context.write(keyOut, one);
+            	}
+            }
+        }
     }
 
+    /**
+     * Reducer Implementation for WordCount Job
+     *
+     * Input:  key    - a word (type: Text)
+     *         values - a list of 1 (for each document the word occurs) (type: IntWritable)
+     * Output: key    - a word (type: Text)
+     *         value  - IDF score of this word (type: DoubleWritable)
+     */
     public static class IDFReducer extends Reducer<Text, IntWritable, Text, DoubleWritable> {
         // you may assume that this variable contains the total number of documents.
         private int totalDocuments;
@@ -71,7 +111,22 @@ public class InverseDocumentFrequency extends Configured implements Tool {
             totalDocuments = context.getConfiguration().getInt("totalDocuments", 1);
         }
 
-        // fill your code here!
+        private DoubleWritable valueOut = new DoubleWritable();
+
+        @Override
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            // sum the all received counts (using a loop since Iterable has no length property)
+            int sum = 0;
+            for (IntWritable count : values) {
+                sum++;
+            }
+            
+            double idfScore = Math.log((double)totalDocuments / sum);
+            valueOut.set(idfScore);
+
+            // write a (word, idfScore) pair to output file.
+            context.write(key, valueOut);
+        }
     }
 }
 
